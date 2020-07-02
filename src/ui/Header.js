02bbbdb1c5c0c9d24b82a3695713139ac3659c06
Dave Evans <codingdave.dev@gmail.com>
2020-07-02 17:38:19 -0500

    Starter Files

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -15,6 +15,7 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useTheme } from "@material-ui/core/styles";
 
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -203,44 +204,11 @@ const Header = ({
       link: "/listing/newListing",
       activeIndex: 5,
       selectedIndex: 0,
-    },
-    {
-      name: "Your Favourites",
-      link: `/user/userFavourites?id=${profile.uid}`,
-      activeIndex: 5,
-      selectedIndex: 1,
-    },
-    {
-      name: "Your Listings",
-      link: `/user/userListings?id=${profile.uid}`,
-      activeIndex: 5,
-      selectedIndex: 2,
-    },
-    {
-      name: "Checkins",
-      link: `/user/userCheckins?id=${profile.uid}`,
-      activeIndex: 5,
-      selectedIndex: 3,
-    },
-    {
-      name: "Profile",
-      link: `/profile/userProfile?id=${profile.uid}`,
-      activeIndex: 5,
-      selectedIndex: 4,
-    },
-    {
-      name: "Sign Out",
-      link: '/logout',
-      activeIndex: 5,
-      selectedIndex: 5,
-    },
+    }
   ];
 
   const routes = [
     { name: "Home", link: "/", activeIndex: 0 },
-    { name: "About Us", link: "/about", activeIndex: 1 },
-    { name: "The Board", link: "/the_board", activeIndex: 2 },
-    { name: "Listings", link: "/listings", activeIndex: 3 },
 
   ];
 
@@ -249,16 +217,7 @@ const Header = ({
       name: "Admin Area",
       link: "/admin",
       activeIndex: 4,
-    },
-    {
-      name: profile.fullName,
-      link: `/profile/userProfile?id=${profile.uid}`,
-      activeIndex: 5,
-      ariaOwns: anchorEl ? "simple-menu" : undefined,
-      ariaPopup: anchorEl ? "true" : undefined,
-      mouseOver: (event) => handleClick(event),
-      end: true,
-    },
+    }
   ];
 
   const buttonRoutes = [
