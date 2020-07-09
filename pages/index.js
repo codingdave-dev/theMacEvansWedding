47d355557810000df4885c7bb4fc5632e29a4573
Dave Evans <codingdave.dev@gmail.com>
2020-07-08 19:07:17 -0500

    UPDATE FIREBASE CONFIG

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 const Index = () => {
   const classes = useStyles();
   const theme = useTheme();
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,14 +5,15 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
+    apiKey: `${process.env.API_KEY}`,
+    authDomain: `${process.env.AUTH_DOMAIN}`,
+    databaseURL: `${process.env.DATABASE_URL}`,
+    projectId: `${process.env.PROJECT_ID}`,
+    storageBucket: `${process.env.STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+    appId: `${process.env.APP_ID}`,
 };
+
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
